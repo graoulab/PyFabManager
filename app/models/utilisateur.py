5d0d526d1f3078b9ba7448bdb346c4854abed223
Benjamin Thiebaut <benjamin.thibaut@rsiconcept.fr>
2015-11-04 12:23:03 +0100

    modif user model

--- app/models/utilisateur.py
+++ app/models/utilisateur.py
@@ -1,9 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 from .autre import TypeAdherent
 class utilisateur(models.Model):
-	Nom = models.CharField(max_length=30)
-	Prenom = models.CharField(max_length=30)
-	Email = models.EmailField(max_length=254)
-	DateDeCreation = models.DateTimeField(auto_now=False, auto_now_add=False)
+	user = models.OneToOneField(User, unique=True)
 	UserType = models.ForeignKey('TypeAdherent')
-	rang = models.IntegerField()
